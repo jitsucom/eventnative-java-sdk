f94123d6d5eeadfca5c016a6c5cb2bd41c4bc75d
akhtyamovrr <ruslan@ksense.ai>
2020-09-11 18:03:19 +0300

    Added javadocs to Async clients

--- src/main/java/org/eventnative/client/async/C2SAsyncClient.java
+++ src/main/java/org/eventnative/client/async/C2SAsyncClient.java
@@ -6,6 +6,9 @@ import okhttp3.OkHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This client adds events to the queue that is processed by a dedicated thread which sends events to backend.
+ */
 public class C2SAsyncClient extends AsyncEventClient {
 
     private static final String C2S_EVENT_URL = "/api/v1/event?token=";
@@ -19,6 +22,14 @@ public class C2SAsyncClient extends AsyncEventClient {
         super(baseUrl, C2S_EVENT_URL, token, client, queueCapacity);
     }
 
+    /**
+     * Puts event to the queue. Separate thread sends events to server.
+     *
+     * @param eventContext JSON object with event context content
+     * @param properties   JSON object with all the properties except for event context
+     * @return true if event was added to the queue, false if the queue is full.
+     * @throws IllegalArgumentException if eventContext is null
+     */
     public boolean sendEvent(JsonObject eventContext, JsonObject properties) {
         logger.debug("event context: [{}], other properties: [{}]", eventContext, properties);
         if (eventContext == null) {
--- src/main/java/org/eventnative/client/async/S2SAsyncEventClient.java
+++ src/main/java/org/eventnative/client/async/S2SAsyncEventClient.java
@@ -5,6 +5,9 @@ import okhttp3.OkHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This client adds any events to the queue that is processed by a dedicated thread which sends events to backend.
+ */
 public class S2SAsyncEventClient extends AsyncEventClient {
     private static final String S2S_EVENT_URL = "/api/v1/s2s/event?token=";
     private final Logger logger = LoggerFactory.getLogger(S2SAsyncEventClient.class);
@@ -17,6 +20,13 @@ public class S2SAsyncEventClient extends AsyncEventClient {
         super(baseUrl, S2S_EVENT_URL, token, client, queueCapacity);
     }
 
+    /**
+     * Puts event to the queue. Separate thread sends events to server.
+     *
+     * @param event any JSON object
+     * @return true if the event was added to the queue, false if the queue if full
+     * @throws IllegalArgumentException if event is null
+     */
     public boolean sendEvent(JsonObject event) {
         if (event == null) {
             throw new IllegalArgumentException("event should be a valid JSON object");
